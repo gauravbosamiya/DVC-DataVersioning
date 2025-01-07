9a6fecb78e5c0bd363e530fb877b186a21f8f40f
Gaurav <gauravbosamiya9@gmail.com>
2025-01-07 19:34:08 +0530

    first commit before DVC

---
+++ mycode.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+data = {'Name':['Gaurav','Nirav', 'Alice'],
+        'Age':[25,30,35],
+        'City':['New York','Los Angeles','Chicago']}
+
+df = pd.DataFrame(data)
+
+## Adding new row to df for V2
+# new_row = {'Name':'V2','Age':20,'City':'City1'}
+# df.loc[len(df.index)] = new_row
+
+## Adding new row to df for V3
+# new_row2= {'Name':'V3','Age':20,'City':'City1'}
+# df.loc[len(df.index)] = new_row2
+
+data_dir = "data"
+os.makedirs(data_dir,exist_ok=True)
+
+file_path = os.path.join(data_dir,'sample_data.csv')
+
+df.to_csv(file_path, index=False)
+
+print(f"csv file saved to {file_path}")
